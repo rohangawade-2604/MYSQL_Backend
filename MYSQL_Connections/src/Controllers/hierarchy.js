24f28876933467988472d97ba128c733b32f9668
rohangawade-2604 <rohangawade2604@gmail.com>
2026-03-16 11:10:53 +0530

    code

--- MYSQL_Connections/src/Controllers/hierarchy.js
+++ MYSQL_Connections/src/Controllers/hierarchy.js
@@ -1,8 +1,7 @@
 const XLSX = require("xlsx");
 const fs = require("fs");
-const { Connection } = require("../../config/db"); // ensure this is mysql2/promise
+const { Connection } = require("../../config/db");
 
-// Promisify query for easier async/await
 const queryAsync = (sql, params) =>
   new Promise((resolve, reject) => {
     Connection.query(sql, params, (err, result) => {
@@ -12,6 +11,7 @@ const queryAsync = (sql, params) =>
   });
 
 const uploadHierarchyExcel = async (req, res) => {
+  
   try {
     if (!req.file) {
       return res.status(400).json({ message: "Excel file required" });
@@ -19,12 +19,6 @@ const uploadHierarchyExcel = async (req, res) => {
 
     const workbook = XLSX.readFile(req.file.path);
     const sheetName = workbook.SheetNames[0];
-
-    if (!sheetName) {
-      return res
-        .status(400)
-        .json({ message: "No sheet found in excel file" });
-    }
 
     const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
 
@@ -49,72 +43,39 @@ const uploadHierarchyExcel = async (req, res) => {
         MRName,
         MRPassword,
         MRHq,
-        MRZone,
+        MRZone
       } = row;
 
-      
-      // Insert TLM (ignore if duplicate)
-      const tlmQuery = `
-        INSERT IGNORE INTO TLM
-        (TLMID, TLMName, TLMPassword, TLMHq, TLMZone, SLMID)
-        VALUES (?, ?, ?, ?, ?, ?)
-      `;
-      await queryAsync(tlmQuery,  [
-        TLMID,
-        TLMName,
-        TLMPassword,
-        TLMHq,
-        TLMZone,
-        SLMID
-      ]);
-      console.log(`TLM Inserted: ${TLMID}`);
+      await queryAsync(
+        `INSERT IGNORE INTO TLM (TLMID,TLMName,TLMPassword,TLMHq,TLMZone,SLMID)
+         VALUES (?,?,?,?,?,?)`,
+        [TLMID,TLMName,TLMPassword,TLMHq,TLMZone,SLMID]
+      );
 
-      // Insert SLM
-      const slmQuery = `
-        INSERT IGNORE INTO SLM
-        (SLMID, SLMName, SLMPassword, SLMHq, SLMZone, FLMID)
-        VALUES (?, ?, ?, ?, ?, ?)
-      `;
-      await queryAsync(slmQuery,  [SLMID, SLMName, SLMPassword, SLMHq, SLMZone, FLMID]);
-      console.log(`SLM Inserted: ${SLMID}`);
+      await queryAsync(
+        `INSERT IGNORE INTO SLM (SLMID,SLMName,SLMPassword,SLMHq,SLMZone,FLMID)
+         VALUES (?,?,?,?,?,?)`,
+        [SLMID,SLMName,SLMPassword,SLMHq,SLMZone,FLMID]
+      );
 
-      // Insert FLM
-      const flmQuery = `
-        INSERT IGNORE INTO FLM
-        (FLMID, FLMName, FLMPassword, FLMHq, FLMZone, MRID)
-        VALUES (?, ?, ?, ?, ?, ?)
-      `;
-      await queryAsync(flmQuery, [
-        FLMID,
-        FLMName,
-        FLMPassword,
-        FLMHq,
-        FLMZone,
-        MRID
-      ]);
-      console.log(`FLM Inserted: ${FLMID}`);
+      await queryAsync(
+        `INSERT IGNORE INTO FLM (FLMID,FLMName,FLMPassword,FLMHq,FLMZone,MRID)
+         VALUES (?,?,?,?,?,?)`,
+        [FLMID,FLMName,FLMPassword,FLMHq,FLMZone,MRID]
+      );
 
-      // Insert MR
-      const mrQuery = `
-        INSERT IGNORE INTO MR
-        (MRID, MRName, MRPassword, MRHq, MRZone)
-        VALUES (?, ?, ?, ?, ?)
-      `;
-      await queryAsync(mrQuery, [MRID, MRName, MRPassword, MRHq, MRZone]);
-      console.log(`MR Inserted: ${MRID}`);
+      await queryAsync(
+        `INSERT IGNORE INTO MR (MRID,MRName,MRPassword,MRHq,MRZone)
+         VALUES (?,?,?,?,?)`,
+        [MRID,MRName,MRPassword,MRHq,MRZone]
+      );
     }
 
-
-
-    // Delete uploaded file asynchronously
-    fs.unlink(req.file.path, (err) => {
-      if (err) console.log("Failed to delete file:", err);
-      else console.log("Uploaded Excel file deleted");
-    });
+    fs.unlink(req.file.path, () => {});
 
     res.status(200).json({ message: "Excel data imported successfully" });
+
   } catch (error) {
-    console.error("Excel Import Error:", error);
     res.status(500).json({ message: "Error importing excel", error: error.message });
   }
 };
