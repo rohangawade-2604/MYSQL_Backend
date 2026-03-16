f24e10f82d78e8f0aa3493074d80c1994d1ae1a7
rohangawade-2604 <rohangawade2604@gmail.com>
2026-03-16 18:09:59 +0530

    code

---
+++ MYSQL_Connections/src/Controllers/fullhierarchy.js
@@ -0,0 +1,35 @@
+const {queryAsync} = require("../Controllers/hierarchy.js")
+
+const fullhierarchy = async() => {
+
+    try {
+        
+        const query = `SELECT 
+        T.TLMID, T.TLMName, T.TLMPassword, T.TLMHq, T.TLMZone,
+        S.SLMID, S.TLMName, S.SLMPassword, S.SLMHq, S.SLMZone,
+        F.FLMID, F.FLMName, F.FLMPassword, F.FLMHq, F.FLMZone,
+        M.MRID, M.MRName, M.MRPassword, M.MRHq, M.MRZone,
+
+        FROM TLM T
+        LEFT JOIN SLM S ON T.SLMID = S.SLMID
+        LEFT JOIN FLM F ON S.FLMID = F.FLMID
+        LEFT JOIN MR M ON F.MLMID = M.MRID
+        `;
+
+        const result = await queryAsync(query);
+
+        return res.json({
+            success: true,
+            count: result.length;
+            data: result
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: "error is been occured",
+            error: error.message
+        })
+    }
+}
+
+module.exports = {fullhierarchy}
